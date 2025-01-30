6f97f002983d060f8f874abee6d6e05232d15103
antoine_dibartolomeo <antoine.dibartolomeo.ext@orange.com>
2025-01-30 11:38:26 +0100

    files

---
+++ files/file_tutorial.py
@@ -0,0 +1,25 @@
+
+# students_list = ["Paul","Louis", "Mathieu", "Edouard","Laurent", "Maxime"]
+#
+# with open("student.txt","w+") as file:
+#     for student in students_list:
+#         file.write(student + "\n")
+#     file.close()
+import os
+import random
+import shutil
+
+if os.path.exists("meals.txt"):
+    with open("meals.txt","r+") as file:
+        meals_list = file.readlines()
+        meal_random_choice =  random.choice(meals_list)
+        print("Je vous propose aujourd'hui, le repas " + meal_random_choice)
+        file.close()
+else:
+    print("Le document n'existe pas ! attention!")
+
+
+source = "logo.ico"
+target = "images/logo.ico"
+shutil.copy(source,target)
+os.remove(source)
